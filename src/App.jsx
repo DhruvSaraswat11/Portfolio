bf909cb90e3f3bae91748daf30688dd02cf29a38
Dhruv Saraswat <dhruvsaraswat22102006@gmail.com>
2025-10-16 22:18:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.scss";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import Parallex from "./components/parallex/Parallex";
-import Services from "./components/services/Services";
+import Services from "./components/services/Services.jsx";
 import Portfolio from "./components/portfolio/Portfolio";
 import Contact from "./components/contact/Contact";
 import About from "./components/about/About";
