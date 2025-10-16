a3a0dfe2ae04dba6070eff478ea71b710ef0b6e5
Dhruv Saraswat <dhruvsaraswat22102006@gmail.com>
2025-10-16 22:34:55 +0530

    Update Parallex.jsx

--- src/components/parallex/Parallex.jsx
+++ src/components/parallex/Parallex.jsx
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import "./Parallex.scss";
+import "./parallex.scss";
 import { motion, useScroll, useTransform } from "framer-motion";
 const Parallex = ({ type }) => {
   const ref = useRef(null);
@@ -40,3 +40,4 @@ const Parallex = ({ type }) => {
   );
 };
 export default Parallex;
+
